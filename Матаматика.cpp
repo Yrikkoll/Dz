8407a926b7baa4240501896b3949357c235f063b
Yrikkoll <yrik1980koll@gmail.com>
2025-04-13 01:49:57 +0300

    Update Матаматика.cpp

--- Матаматика.cpp
+++ Матаматика.cpp
@@ -1,17 +1,11 @@
 #include <iostream>  
-
 int main() {
     int a, b;
-
-    
-    std:cout << "Enter a: "<<;
-    std:cout <<"Enter b: "<<;
-    std:cin >> a >> b;
-
-    
-    std::cout << "+: " << (a + b) << std::endl;
-    std::cout << "-: " << (a - b) << std::endl;
-    std::cout << "*: " << (a * b) << std::endl;
-
-    return 0;  
+    std:cout << "Enter a: "<<
+        std:cout <<"Enter b: "<<;
+     std:cin >> a >> b;
+     std:cout << "+: " << (a + b) << std:endl;
+     std:cout << "-: " << (a - b) << std:endl;
+     std:cout << "*: " << (a * b) << std:endl;
+     return 0;  
 }
