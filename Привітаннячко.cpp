79cdd53d56adfea4b9960c8c223866008f3befd8
Yrikkoll <yrik1980koll@gmail.com>
2025-04-13 01:17:31 +0300

    Update Привітаннячко.cpp

--- Привітаннячко.cpp
+++ Привітаннячко.cpp
@@ -1,13 +1,1 @@
-#include <iostream>  
-#include <string>    
 
-int main() {
-    std::string name;  
-
-    std::cout << "Введіть ваше ім'я: ";  
-    std::getline(std::cin, name);  
-
-    std::cout << "Як себе почуваєш, " << name << "?" << std::endl;  
-
-    return 0;  
-}
