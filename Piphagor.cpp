4be816506e839b0913029ac472192b29f3702f6d
Yrikkoll <yrik1980koll@gmail.com>
2025-04-13 01:23:55 +0300

    Update Piphagor.cpp

--- Piphagor.cpp
+++ Piphagor.cpp
@@ -1,11 +1,1 @@
-#include <iostream>
-#include <cmath>
-struct point{
-    double x,y,z;
-    point(double x, double y, double z) : x(x), y(y), z(z) {}
-    double distance(const point& other) const {
-        return std::sqrt(std::pow(x - other.x,2) +
-                         std::pow(y - other.y,2) +
-                         std::pow(z - other.z,2));
-    }
-}:
+
