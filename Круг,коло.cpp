01403b6ab32540a89b52cf04f7d09e357c1888ab
Yrikkoll <yrik1980koll@gmail.com>
2025-05-07 20:27:38 +0300

    Круг,коло.cpp

--- Круг,коло.cpp
+++ Круг,коло.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
 
+int main(){
+
+	int a,b,c,pl,ob;
+	std::cin >> a >> b >> c;
+	pl = 2*(a*b+a*c+b*c);
+	ob = a*b*c;
+	std::cout << "ob par: " << ob << ", pl par: " << pl;
+
+
+}
