efb769451f3f5b35ae5eef64651e3bb12f3f265f
Yrikkoll <yrik1980koll@gmail.com>
2025-05-07 20:27:11 +0300

    Math.cpp

--- Piphagor.cpp
+++ Piphagor.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
 
+int main(){
+
+	int a,b;
+	std::cin >> a >> b;
+	std::cout << "s = " << a+b << std::endl;
+	std::cout << "r = " << a-b << std::endl;
+	std::cout << "d = " << a*b << std::endl;
+
+
+}
