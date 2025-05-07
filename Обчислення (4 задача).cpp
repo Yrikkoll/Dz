a9184f8a0812bb63adabfe644a5b1c246deae2c3
Yrikkoll <yrik1980koll@gmail.com>
2025-05-07 20:31:24 +0300

    Відсотки

--- Обчислення (4 задача).cpp
+++ Обчислення (4 задача).cpp
@@ -1,10 +1,16 @@
-#include <iostream>  
+# include <iostream>
 
-int main() {  
-int: A;
-  cout:<<"Enter A:"
-    cin >> A
- std:cout << "S: " << (6A^2) << std:endl;
-return 0;  
-}  
-
+int main(){
+	float P,R,I;
+	int T;
+	std::cout << "input your suffen: "<< '\n';
+	std::cin >> P;
+	std::cout << "input your interest rates: "<< '\n';
+	std::cin >> R;
+	std::cout << "input your loan period: "<< '\n';
+       	std::cin >> T;
+	I = (P*T*R)/100;
+	int a = I;
+	std::cout <<"result': "<<  a << '\n';
+	std::cout << "result: " << I << '\n';
+}
