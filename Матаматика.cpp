d8e354312b00891a018824b6568c51ed7dfc2cf2
Yrikkoll <yrik1980koll@gmail.com>
2025-05-07 20:29:00 +0300

    Radius

--- Матаматика.cpp
+++ Матаматика.cpp
@@ -1,11 +1,16 @@
-#include <iostream>  
-int main() {
-    int a, b;
-    std:cout << "Enter a: "<<
-        std:cout <<"Enter b: "<<;
-     std:cin >> a >> b;
-     std:cout << "+: " << (a + b) << std:endl;
-     std:cout << "-: " << (a - b) << std:endl;
-     std:cout << "*: " << (a * b) << std:endl;
-     return 0;  
+#include <iostream>
+
+int main(){
+	
+	float p = 3.14;
+	float ra;
+	std::cout << "enter radius: ";
+	std::cin >> ra;
+	float pl = 2*p*ra*ra;
+	float d = p*ra*ra;
+	int n = pl;
+	int a = d;
+	std::cout << "ploshca kola: "<< n << "." << int(pl* 100) % (n*10) << '\n';
+	std::cout << "dovzina kola: "<< a << "." << int(d*100) % (a*10) <<'\n';
+
 }
